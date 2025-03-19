e235171d6a921c4f8229876c82a5deb18a47fcf0
Connor Valley <valleyco@mail.gvsu.edu>
2025-03-19 19:54:03 -0400

    remove commented out print statements

--- itnr.py
+++ itnr.py
@@ -20,23 +20,19 @@ def check_cache(url, domain):
   subdomain = url_parts[-2] + "." + domain
   
   if ("ip:"+url) in cache:
-    #print(f"Cache hit")
     return cache["ip:"+url]
-  elif ("ns:"+subdomain) in cache:
-    #print(f"Cache hit for {subdomain}")    
+  elif ("ns:"+subdomain) in cache:   
     print(f"Querying {subdomain} name servers from cache for {url}")
 
     ip = query_ns(url, cache["ns:"+subdomain], domain)
     return ip
   elif ("tld:"+domain) in cache:
-    #print(f"Cache hit for domain {domain}")
     print(f"Querying {domain} tlds from cache for {subdomain}")
 
     name_servers = query_tld(subdomain, cache["tld:"+domain])
     ip = query_ns(url, name_servers, domain)
     return ip
   else:
-    #print("No results found in cache, querying root servers")
     tlds = query_root(url, domain)
 
     if not tlds:
@@ -65,7 +61,6 @@ def query_root(url, domain):
         tlds.append(additional[tld])
 
     if tlds:
-      #print(f"Found {len(tlds)} TLDs for {domain}")
       cache["tld:"+domain] = tlds
       return tlds
 
@@ -81,7 +76,6 @@ def query_tld(subdomain, tlds):
     answers, name_servers, additional = get_dns_record(sock, subdomain, tld, "NS")
     
     if name_servers:
-      #print(f"Found {len(name_servers)} name servers for {subdomain}")
       cache["ns:"+subdomain] = name_servers
       return name_servers
   
@@ -94,7 +88,6 @@ def query_ns(url, name_servers, domain):
 
     for a in answers:
       if a.rtype == QTYPE.A:
-        #print(f"Found result from {ns} for {url}: {a.rdata}")
         ip = str(a.rdata)
         return ip
       elif a.rtype == QTYPE.CNAME:
@@ -112,7 +105,6 @@ def query_ns(url, name_servers, domain):
 def get_dns_record(udp_socket, domain:str, parent_server: str, record_type):
   q = DNSRecord.question(domain, qtype = record_type)
   q.header.rd = 0   # Recursion Desired?  NO
-  #print("DNS query", repr(q))
   udp_socket.sendto(q.pack(), (parent_server, DNS_PORT))
   pkt, _ = udp_socket.recvfrom(8192)
   buff = DNSBuffer(pkt)
@@ -123,7 +115,6 @@ def get_dns_record(udp_socket, domain:str, parent_server: str, record_type):
   
   try:
     header = DNSHeader.parse(buff)
-    #print("DNS header", repr(header))
     if q.header.id != header.id:
       print("Unmatched transaction")
       return
@@ -135,12 +126,10 @@ def get_dns_record(udp_socket, domain:str, parent_server: str, record_type):
     # Parse the question section #2
     for k in range(header.q):
       q = DNSQuestion.parse(buff)
-      #print(f"Question-{k} {repr(q)}")
 
     # Parse the answer section #3
     for k in range(header.a):
       a = RR.parse(buff)
-      #print(f"Answer-{k} {repr(a)}")
       answers.append(a)
       if a.rtype == QTYPE.A:
         answers.append(a.rdata)
@@ -148,14 +137,12 @@ def get_dns_record(udp_socket, domain:str, parent_server: str, record_type):
     # Parse the authority section #4
     for k in range(header.auth):
       auth = RR.parse(buff)
-      #print(f"Authority-{k} {repr(auth)}")
       if (auth.rtype == QTYPE.NS):
         name_servers.append(str(auth.rdata))
 
     # Parse the additional section #5
     for k in range(header.ar):
       adr = RR.parse(buff)
-      #print(f"Additional-{k} {repr(adr)} Name: {adr.rname}")
       if (adr.rtype == QTYPE.A):
         additional[str(adr.rname)] = str(adr.rdata)
 
